d46f4dc2e09437ac9859b16e764280e3f25a6f95
AndresOrtega-tech <tamezandres191@gmail.com>
2026-04-20 19:33:13 -0600

    fix: add missing origin to CORS configuration

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ origins = [
     "http://localhost:3000",
     "http://localhost:8081",
     "https://web-app-organiza-t.vercel.app",
+    "https://web-app-organiza-t-git-development-andresortegatechs-projects.vercel.app/"
 ]
 
 app.add_middleware(
